7724a7c35b863b925d7bbd3a0b14ef28e59f3da9
dwilliams304 <d.williams0434@gmail.com>
2023-09-07 12:37:56 -0700

    Finished

---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,36 @@
+import { TOGGLE_FAVORITES, REMOVE_FAVORITE, ADD_FAVORITE } from "../actions/favoritesActions";
+
+
+const initialState = {
+    favorites: [],
+    displayFavorites: true
+}
+
+
+const favoritesReducer = (state = initialState, action) => {
+    switch(action.type){
+        case(TOGGLE_FAVORITES):
+            return({
+                ...state,
+                displayFavorites: !state.displayFavorites
+            })
+        
+        case(ADD_FAVORITE):
+            return({
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            })
+
+        case(REMOVE_FAVORITE):
+            return({
+                ...state,
+                favorites: state.favorites.filter(i => i.id !== action.payload)
+            })       
+        
+        default:
+            return(state)
+    }
+}
+
+
+export default favoritesReducer;
